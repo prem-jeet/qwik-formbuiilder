720364f78d27735e3febc0eefd89b72442a6bab7
prem-jeet <jeet4300795@gmail.com>
2023-11-02 01:04:22 +0530

    added left menu to search for field and trigger a function that adds new input to the formlayout array

---
+++ src/components/available-fields-menu/availableFieldMenu.tsx
@@ -0,0 +1,67 @@
+import type { Signal } from "@builder.io/qwik";
+import { component$, useComputed$, useSignal } from "@builder.io/qwik";
+
+export type AvailableInputTypes =
+  | "text"
+  | "select"
+  | "checkbox"
+  | "textaea"
+  | "number"
+  | "column"
+  | "section";
+
+const availableInputType: AvailableInputTypes[] = [
+  "text",
+  "select",
+  "checkbox",
+  "textaea",
+  "number",
+  "column",
+  "section",
+];
+interface Props {
+  selectedInput: Signal<AvailableInputTypes | null>;
+}
+export default component$<Props>(({ selectedInput }) => {
+  const fieldSearchFilter = useSignal("");
+
+  const filteredAvailableInputTypes = useComputed$(() => {
+    return availableInputType.filter((type) =>
+      type.includes(fieldSearchFilter.value.toLocaleLowerCase())
+    );
+  });
+  return (
+    <>
+      <div>
+        <div class="input-group mb-3">
+          <span class="input-group-text ">
+            <i class="bi bi-search"></i>
+          </span>
+          <input
+            type="text"
+            class="form-control"
+            placeholder="search fields"
+            bind:value={fieldSearchFilter}
+          />
+        </div>
+        <div>
+          <div class="row row-cols-2 gy-3">
+            {filteredAvailableInputTypes.value.map((type) => (
+              <>
+                <div class="col">
+                  <button
+                    type="button"
+                    class="w-100 btn btn-light border border-2 "
+                    onClick$={() => (selectedInput.value = type)}
+                  >
+                    {type.toLocaleUpperCase()}
+                  </button>
+                </div>
+              </>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+});
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,49 @@
-import { component$ } from "@builder.io/qwik";
+import type { AvailableInputTypes } from "~/components/available-fields-menu/availableFieldMenu";
 import type { DocumentHead } from "@builder.io/qwik-city";
+import { $, component$, useSignal, useTask$ } from "@builder.io/qwik";
+import AvailableFieldMenu from "~/components/available-fields-menu/availableFieldMenu";
+
+interface InputObject {
+  type: AvailableInputTypes;
+  label: string | null;
+  name: string | null;
+  mendatory: boolean;
+}
+
 export default component$(() => {
+  const formLayout = useSignal<InputObject[]>([]);
+  const selectedInputType = useSignal<AvailableInputTypes | null>(null);
+  const insertIntoFormLayout = $((type: AvailableInputTypes) => {
+    const newInputTypeObject: InputObject = {
+      name: null,
+      label: null,
+      mendatory: false,
+      type: type,
+    };
+    formLayout.value = [...formLayout.value, { ...newInputTypeObject }];
+  });
+
+  useTask$(({ track }) => {
+    track(() => selectedInputType.value);
+    if (selectedInputType.value) {
+      insertIntoFormLayout(selectedInputType.value);
+      selectedInputType.value = null;
+    }
+  });
+
   return (
     <>
-      <div class="alert alert-primary" role="alert">
-        A simple primary alert—check it out!
+      <div class="vw-100 vh-100 overflow-hidden">
+        <div class="container">
+          <div class="row py-5 vh-100 justify-content-between">
+            <div class="col-3">
+              <div>
+                <AvailableFieldMenu selectedInput={selectedInputType} />
+              </div>
+            </div>
+            <div class="col">{JSON.stringify(formLayout.value, null, 2)}</div>
+          </div>
+        </div>
       </div>
     </>
   );

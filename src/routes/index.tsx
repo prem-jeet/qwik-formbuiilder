13e2c43cf7f5ace96c74e9f859724bcf53b57ed8
prem-jeet <jeet4300795@gmail.com>
2023-11-02 16:22:17 +0530

    added form layout display

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,23 +2,28 @@ import type { AvailableInputTypes } from "~/components/available-fields-menu/ava
 import type { DocumentHead } from "@builder.io/qwik-city";
 import { $, component$, useSignal, useTask$ } from "@builder.io/qwik";
 import AvailableFieldMenu from "~/components/available-fields-menu/availableFieldMenu";
+import FormLayoutDIsplay from "~/components/formLayoutDisplay/formLayoutDIsplay";
 
-interface InputObject {
+export interface InputObject {
   type: AvailableInputTypes;
   label: string | null;
   name: string | null;
   mendatory: boolean;
+  id: string;
 }
 
 export default component$(() => {
-  const formLayout = useSignal<InputObject[]>([]);
   const selectedInputType = useSignal<AvailableInputTypes | null>(null);
+  const isPreview = useSignal(false);
+
+  const formLayout = useSignal<InputObject[]>([]);
   const insertIntoFormLayout = $((type: AvailableInputTypes) => {
     const newInputTypeObject: InputObject = {
       name: null,
       label: null,
       mendatory: false,
       type: type,
+      id: (() => crypto.randomUUID())(),
     };
     formLayout.value = [...formLayout.value, { ...newInputTypeObject }];
   });
@@ -41,7 +46,25 @@ export default component$(() => {
                 <AvailableFieldMenu selectedInput={selectedInputType} />
               </div>
             </div>
-            <div class="col">{JSON.stringify(formLayout.value, null, 2)}</div>
+            <div class="col px-5">
+              <div>
+                <div class="d-flex justify-content-end  mb-4">
+                  <button
+                    type="button"
+                    class="btn btn-primary"
+                    onClick$={() => (isPreview.value = !isPreview.value)}
+                  >
+                    {isPreview.value ? "Hide " : "Show "}Preview
+                  </button>
+                </div>
+                <div>
+                  <FormLayoutDIsplay
+                    formLayout={formLayout}
+                    isPreview={isPreview}
+                  />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
